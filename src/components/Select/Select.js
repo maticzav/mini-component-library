19b41737680bf129f6961941fffe2ca16ef4f944
Matic Zavadlal <matic.zavadlal@gmail.com>
2022-09-19 17:06:59 +0200

    implement select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,75 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <CustomSelect>
+        {displayedValue}
+        <IconWrapper style={{ "--size": 24 + "px" }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </CustomSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  -webkit-appearance: none;
+  opacity: 0;
+`;
+
+const CustomSelect = styled.div`
+  height: 100%;
+
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+
+  right: 10px;
+
+  height: var(--size);
+  width: var(--size);
+
+  pointer-events: none;
+`;
 
 export default Select;
